59ac6a1e988c0908693e69558811ded9576c9646
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-09 23:43:09 +0900

    出力文

---
+++ systemout.java
@@ -0,0 +1,9 @@
+public class systemout{
+  public static void main(String[] args){
+    System.out.println("Q1:ベトナム");
+    System.out.println("1:東京");
+    System.out.println("2:ハノイ");
+    System.out.println("3:ソウル");
+    System.out.println("4:マニラ");
+  }
+}
