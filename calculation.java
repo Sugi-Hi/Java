9506d8e46712b7f4dcd6a5fb1c5a3ea94e4aeb0d
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-15 14:13:31 +0900

    演算計算

--- calculation.java
+++ calculation.java
@@ -2,6 +2,12 @@ public class calculation{
   public static void main(String[] args){
     int x=100;
     int y=200;
-    System.out.println(x+y);
+
+    System.out.println("加算："+(x+y));
+    System.out.println("引算："+(x-y));
+    System.out.println("掛算："+(x*y));
+    System.out.println("割算："+(x/y));
+    System.out.println("剰余："+(x%y));
+
   }
 }
