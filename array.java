0dc76884721de44297d26fe0018fe753110455c8
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-16 18:44:42 +0900

    配列計算・出力

--- array.java
+++ array.java
@@ -1,9 +1,12 @@
 public class array{
+  public int[] intArray={ 1, 200, -1, 50, 33 , -15};
+
   public static void main(String[] args){
-    int[] intArray = new int[6];
+    // int[] intArray = new int[6];  2回宣言エラー
+
+    int[] intArray={ 1, 200, -1, 50, 33 , -15};
     System.out.println(intArray.length);
 
-    // intArray = {1, 200, -1, 50, 33, -15};
     intArray[2] = 5;
     intArray[0] -= 3;
     intArray[1] = intArray[2]*4;
