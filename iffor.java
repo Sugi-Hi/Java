213d634ba6a8272994e95e023568e2eb7736747c
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-18 23:07:29 +0900

    条件繰返

---
+++ iffor.java
@@ -0,0 +1,15 @@
+public class iffor{
+  public static void main(String[] args){
+    int[] correct = {4,1,2,3,1,2,3,2,4,1};
+    int[] answer = {1,2,3,3,2,4,3,2,1,4};
+
+    System.out.println("あなたのご回答の得点結果");
+    int score = 0;
+    for(int i=0; i<correct.length; i++){
+      if(correct[i]==answer[i]){
+        score ++;
+      }
+    }
+    System.out.println("得点："+(score*10));
+  }
+}
