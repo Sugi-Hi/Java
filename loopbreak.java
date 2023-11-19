f27d160c0b64267571e8cc69842eccda92eff8d7
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-19 21:57:11 +0900

    無限ループ解除

---
+++ loopbreak.java
@@ -0,0 +1,15 @@
+public class loopbreak{
+  public static void main(String[] args){
+
+    int i;
+    int[] data = {1,2,3,3,2,5,4,6,7,1,0,2,7,9,8,2,5};
+
+    for(i=0;i<data.length;i++){
+      if(data[i] == 8){
+        break;
+      }
+    }
+
+    System.out.println("8を発見 " + (i+1));
+  }
+}
