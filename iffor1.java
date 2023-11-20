2904babeb361d324146266048e1938594007e585
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-20 13:09:46 +0900

    条件繰返の修正

--- iffor1.java
+++ iffor1.java
@@ -1,9 +1,9 @@
-public class iffor{
+public class iffor1{
   public static void main(String[] args){
     int[] correct = {4,1,2,3,1,2,3,2,4,1};
     int[] answer = {1,2,3,3,2,4,3,2,1,4};
 
-    System.out.println("あなたのご回答の得点結果");
+    System.out.println("あなたのご回答の得点結果！");
     int score = 0;
     for(int i=0; i<correct.length; i++){
       if(correct[i]==answer[i]){
