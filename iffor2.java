6659153e5b6da96b56396964dae495e509ed00fa
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-20 13:10:34 +0900

    条件繰返2ファイル・実行

---
+++ iffor2.java
@@ -0,0 +1,18 @@
+public class iffor2{
+  public static void main(String[] args){
+    int[] correct = {4,1,2,3,1,2,3,2,4,1};
+    int[] answer = {1,2,3,3,2,4,3,2,1,4};
+
+    System.out.println("あなたのご回答の得点結果です。");
+    int score = 0;
+    for(int i=0; i<correct.length; i++){
+      if(correct[i]==answer[i]){
+        System.out.println((i+1)+"番目：正解！");
+        score ++;
+      }else{
+        System.out.println((i+1)+"番目：不正解");
+      }
+    }
+    System.out.println("得点："+(score*10));
+  }
+}
