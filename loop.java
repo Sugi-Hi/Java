8999a7f498a95c22d4597396abf25b69abaebd59
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-22 17:23:50 +0900

    繰返計算の指定・修正

--- loop.java
+++ loop.java
@@ -1,14 +1,17 @@
 public class loop{
   public static void main(String[] args){
     int i;
-    int sum=0;
-    int mul=1;
+    int sum = 0;
+    long mul = 1;
 
     for(i=1;i<=100;i++){
       sum += i;
+    }
+    for(i=1;i<=10;i++){
       mul *= i;
     }
-      System.out.println("和："+sum);
-      System.out.println("積："+mul);
+
+      System.out.println("和(100)："+sum);
+      System.out.println("積(10 )："+mul);
   }
 }
