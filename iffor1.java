3b24dd3bf5d3c1d89b28cf30e6215b8bb7fdf121
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-24 22:03:43 +0900

    入力番号

--- iffor1.java
+++ iffor1.java
@@ -1,15 +1,22 @@
 public class iffor1{
-  public static void main(String[] args){
-    int[] correct = {4,1,2,3,1,2,3,2,4,1};
-    int[] answer = {1,2,3,3,2,4,3,2,1,4};
+  public static void main(String[] args)
+    throws Exception
+    {
+      int[] correct = {4,1,2,3,1,2,3,2,4,1};
+      int[] answer ={};
 
-    System.out.println("あなたのご回答の得点結果！");
-    int score = 0;
-    for(int i=0; i<correct.length; i++){
-      if(correct[i]==answer[i]){
-        score ++;
+      byte[] b = new byte[128];
+      int len = System.in.read (b);
+  
+      System.out.println("あなたのご回答の得点結果！");
+      int score = 0;
+      for(int i=0; i<correct.length; i++){
+        answer[i] = len;
+        if(correct[i]==answer[i]){
+          score ++;
+        }
       }
+      System.out.print("得点："+(score*10));
+    
     }
-    System.out.println("得点："+(score*10));
-  }
 }
