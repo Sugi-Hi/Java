8313ff2a6eed71eb88a953e0a1cdec69848aec00
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-29 15:53:09 +0900

    文章の指定範囲の修正

--- string1.java
+++ string1.java
@@ -4,9 +4,9 @@ public class string1{
     str = new String("Thank You for your seeing!");
     int index = str.indexOf(' ');
     // String str = new String("Thank You for your seeing!");
-    String str1 = str.substring(0, index);
-    String str2 = str.substring(index+1,index+4);
-    String str3 = str.substring(index+5);
+    String str1 = str.substring(0, index+4);
+    String str2 = str.substring(index+5,index+8);
+    String str3 = str.substring(index+9);
 
     System.out.println("大文字："+str.toUpperCase());
     System.out.print("用　語："+ str +"⇒単語の抜粋："+ str1 + "～" + str2 + "～" + str3 + "\n");
