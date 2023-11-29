801907cc4e7f8d77f61dbe7296d83d2e573f68fe
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-29 15:53:58 +0900

    文章空白の指定範囲・番号

---
+++ string2.java
@@ -0,0 +1,17 @@
+public class string1{
+  public static void main(String[] args){
+    String str;
+    str = new String("Thank You for your seeing!");
+    // String str = new String("Thank You for your seeing!");
+
+    int index  = str.indexOf('!');
+    int index1 = str.indexOf(' ');
+    int index2 = str.indexOf(' ',index1+1);
+    int index3 = str.indexOf(' ',index2+1);
+
+    str = str.substring(0, index);
+
+    System.out.println("大文字："+str.toUpperCase());
+    System.out.print("用　語："+ str +"⇒単語の抜粋："+ index1 + "番目～" + index2 + "番目～" + index3 + "番目\n");
+  }
+}
