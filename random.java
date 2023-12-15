dea8a90f9df5c8ed794e2d8f0438a111eae716e0
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-12-15 10:21:42 +0900

    ランダム国名

--- random.java
+++ random.java
@@ -7,13 +7,19 @@ public class random{
   public static void main(String[] args)
     throws Exception
   {
-    // 国名を格納する ArrayList
+    FileReader fr = new FileReader("nations.txt");
+    BufferedReader br = new BufferedReader(fr);
+    // 国名・首都を格納する ArrayList
     ArrayList<String> nations = new ArrayList<String> ();
+    ArrayList<String> capitals = new ArrayList<String> ();
     Random r = new Random();
+    String correctCapital;
 
     for(int i=0;i<10;i++){
       int qIndex = r.nextInt(nations.size());
       System.out.println(nations.get(qIndex));
+      nations.remove(qIndex);
+      correctCapital = capitals.remove(qIndex);
     }
 
   }
