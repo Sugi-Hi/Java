4d2def5032abf6438875ab7fd243ebafa33ce1d0
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-12-16 22:31:54 +0900

    data作成

---
+++ data.java
@@ -0,0 +1,7 @@
+public class data{
+  public static void main(String[] args){
+    Integer iInt = new Integer(100);
+    Double dDouble = new Double(100.123);
+    System.out.println(iInt + dDouble);
+  }
+}
---
+++ field.java
@@ -0,0 +1,5 @@
+public class field{
+  public static void main(String[] args){
+    
+  }
+}
