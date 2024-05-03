2f836f1d21297dc2cef5a2fbb66fc2366789ea9c
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-03 13:15:11 +0900

    4択番号入出力の改行

--- substring.java
+++ substring.java
@@ -1,24 +1,26 @@
 public class substring{
   public static void main(String[] words)
     throws Exception
+
   {
     int numberOfChoices = 4;
     byte[] b = new byte[128];
     int answer;
 
     while(true){
+
       System.out.print("4択式の番号を入力して下さい!==>");
       System.in.read(b);
       answer = Integer.parseInt((new String(b)).substring(0,1));
-      // System.out.print("\n");
       if(answer >= 1 && answer <= numberOfChoices){
         break;
       }
       else{
-        System.out.println("正しい選択ではありませんので、再入力お願いします。");
+        System.out.print("正しい選択ではありませんので、再入力お願いします。\n");
       }
     }
 
     System.out.println("Answer =>" + answer);
   }
+
 }
