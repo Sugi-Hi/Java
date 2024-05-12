b568a3d536ce5d265e2a7bfd86ce5cf7ed249f48
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-12 23:15:59 +0900

    小数点ケタ数指定

--- data.java
+++ data.java
@@ -3,8 +3,9 @@ public class data{
     Integer iInt = new Integer(100);
     Double dDouble = new Double(100.123);
     Float dFloat = new Float(iInt);
-    System.out.println(dFloat + dDouble);
-    System.out.println(dDouble - dFloat);
+    System.out.println("和:" + (dFloat + dDouble));
+    System.out.print("差:" + (dDouble - dFloat) + "⇒⇒⇒");
+    System.out.printf("小数点第3位まで:%.3f \n",(dDouble - dFloat)); //C言語と同様
 
   }
 }
