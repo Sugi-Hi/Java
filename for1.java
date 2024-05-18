939f4f990762d146a92f9e71b46219485e67057e
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-18 11:03:44 +0900

    首都ファイル更新

--- for1.java
+++ for1.java
@@ -1,6 +1,6 @@
 public class for1{
 
-  public static void main(String[] says){
+  public static void main(String[] cities){
     String[] capitals = {"ハノイ","東京","バンコク","デリー","ジャカルタ"};
     // String[] capitals = new String[50];
     // capitals[0]="ハノイ";
